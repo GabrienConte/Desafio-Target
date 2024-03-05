e5304510113227d24381ee245ba24ebdbef771f8
GabrienConte <gabrielconte0@gmail.com>
2024-03-05 15:13:01 -0300

    Remoçõa dos projetos 3 e 4. Taredfa 2 Concluida

---
+++ Desafio-Target-ConsoleAPP/Desafio-Target-2/Fibonacci.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio_Target_2
+{
+    public static class Fibonacci
+    {
+        private static int valorAnterior = 0;
+        private static int valorAnterior2 = 1;
+        public static bool VerificaNumero(int valor)
+        {
+            bool pertenceFibonacci = false;
+            int proximoValor;
+            while(!pertenceFibonacci && valor >= valorAnterior)
+            {
+                proximoValor = valorAnterior + valorAnterior2;
+                valorAnterior = valorAnterior2;
+                valorAnterior2 = proximoValor;
+
+                if(valor == valorAnterior)
+                    pertenceFibonacci = true;
+            }
+            return pertenceFibonacci;
+        }
+    }
+}
--- Desafio-Target-ConsoleAPP/Desafio-Target-2/Program.cs
+++ Desafio-Target-ConsoleAPP/Desafio-Target-2/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Desafio_Target_2;
+
+int valor;
+Console.WriteLine("Insira um valor para ser encontrado na sequência de Fiboonacci");
+
+bool isValorValido, isFibonacci;
+
+do
+{
+    if(int.TryParse(Console.ReadLine(), out valor))
+    {
+        isValorValido = true;
+        isFibonacci = Fibonacci.VerificaNumero(valor);
+
+        if(isFibonacci)
+            Console.WriteLine(String.Format("O Valor {0} pertence a sequência de Fibonacci!", valor));
+        else
+            Console.WriteLine(String.Format("O Valor {0} NÃO pertence a sequência de Fibonacci!", valor));
+    } else
+    {
+        isValorValido = false;
+        Console.WriteLine("Valor inválido, coloque um número inteiro como entrada");
+    }
+} while(!isValorValido);
